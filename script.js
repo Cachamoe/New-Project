8e32f8dd40129b1ed85a4aa5686a4dae7329bad0
Caleb Chaney <calebc347@gmail.com>
2022-04-12 22:02:21 -0500

    updated style.css

---
+++ script.js
@@ -0,0 +1,25 @@
+function myFunc({
+
+    let b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    b1 = document.getElementById("Box1").value;
+    b1 = document.getElementById("Box2").value;
+    b1 = document.getElementById("Box3").value;
+    b1 = document.getElementById("Box4").value;
+    b1 = document.getElementById("Box5").value;
+    b1 = document.getElementById("Box6").value;
+    b1 = document.getElementById("Box7").value;
+    b1 = document.getElementById("Box8").value;
+    b1 = document.getElementById("Box9").value;
+
+
+
+
+
+
+
+
+
+
+
+
+})
