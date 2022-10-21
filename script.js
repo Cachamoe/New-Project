3156ec627648e1668125ced75157fda4e8b2dd12
Caleb Chaney <calebc347@gmail.com>
2022-10-20 19:22:19 -0500

    Updated Index.html

--- script.js
+++ script.js
@@ -178,9 +178,9 @@ function Game() {
     else if ((b1 == 'X' || b1 == '0') && (b2 == 'X' || b2 == '0') && (b3 == 'X' || b3 == '0') &&
         (b4 == 'X' || b4 == '0') && (b5 == 'X' || b5 == '0') && (b6 == 'X' || b6 == '0') &&
         (b7 == 'X' || b7 == '0') && (b8 == 'X' || b8 == '0') && (b9 == 'X' || b9 == '0')) {
-            document.getElementById('print')
-                .innerHTML = "Match Tie!";
-            window.alert('Match Tie!');
+        document.getElementById('print')
+            .innerHTML = "Match Tie!";
+        window.alert('Match Tie!');
     }
     else {
         // Printing Result
@@ -194,126 +194,126 @@ function Game() {
         }
     }
     // Function to Reset Game
-function Reset() {
-    location.reload();
-    document.getElementById('Box1').value = '';
-    document.getElementById("Box2").value = '';
-    document.getElementById("Box3").value = '';
-    document.getElementById("Box4").value = '';
-    document.getElementById("Box5").value = '';
-    document.getElementById("Box6").value = '';
-    document.getElementById("Box7").value = '';
-    document.getElementById("Box8").value = '';
-    document.getElementById("Box9").value = '';
-}
-// Functions Check Turn of the Player
-flag = 1;
-function Function_3() {
-    if (flag == 1) {
-        document.getElementById("Box1").value = "X";
-        document.getElementById("Box1").disabled = true;
-        flag = 0;
+    function Reset() {
+        location.reload();
+        document.getElementById('Box1').value = '';
+        document.getElementById("Box2").value = '';
+        document.getElementById("Box3").value = '';
+        document.getElementById("Box4").value = '';
+        document.getElementById("Box5").value = '';
+        document.getElementById("Box6").value = '';
+        document.getElementById("Box7").value = '';
+        document.getElementById("Box8").value = '';
+        document.getElementById("Box9").value = '';
     }
-    else {
-        document.getElementById("Box1").value = "0";
-        document.getElementById("Box1").disabled = true;
-        flag = 1;
+    // Functions Check Turn of the Player
+    flag = 1;
+    function Function_3() {
+        if (flag == 1) {
+            document.getElementById("Box1").value = "X";
+            document.getElementById("Box1").disabled = true;
+            flag = 0;
+        }
+        else {
+            document.getElementById("Box1").value = "0";
+            document.getElementById("Box1").disabled = true;
+            flag = 1;
+        }
     }
-}
-function Function_4() {
-    if (flag == 1) {
-        document.getElementById("Box2").value = "X";
-        document.getElementById("Box2").disabled = true;
-        flag = 0;
+    function Function_4() {
+        if (flag == 1) {
+            document.getElementById("Box2").value = "X";
+            document.getElementById("Box2").disabled = true;
+            flag = 0;
+        }
+        else {
+            document.getElementById("Box2").value = "0";
+            document.getElementById("Box2").disabled = true;
+            flag = 1;
+        }
     }
-    else {
-        document.getElementById("Box2").value = "0";
-        document.getElementById("Box2").disabled = true;
-        flag = 1;
+    function Function_5() {
+        if (flag == 1) {
+            document.getElementById("Box3").value = "X";
+            document.getElementById("Box3").disabled = true;
+            flag = 0;
+        }
+        else {
+            document.getElementById("Box3").value = "0";
+            document.getElementById("Box3").disabled = true;
+            flag = 1;
+        }
     }
-}
-function Function_5() {
-    if (flag == 1) {
-        document.getElementById("Box3").value = "X";
-        document.getElementById("Box3").disabled = true;
-        flag = 0;
+    function Function_6() {
+        if (flag == 1) {
+            document.getElementById("Box4").value = "X";
+            document.getElementById("Box4").disabled = true;
+            flag = 0;
+        }
+        else {
+            document.getElementById("Box4").value = "0";
+            document.getElementById("Box4").disabled = true;
+            flag = 1;
+        }
     }
-    else {
-        document.getElementById("Box3").value = "0";
-        document.getElementById("Box3").disabled = true;
-        flag = 1;
+    function Function_7() {
+        if (flag == 1) {
+            document.getElementById("Box5").value = "X";
+            document.getElementById("Box5").disabled = true;
+            flag = 0;
+        }
+        else {
+            document.getElementById("Box5").value = "0";
+            document.getElementById("Box5").disabled = true;
+            flag = 1;
+        }
     }
-}
-function Function_6() {
-    if (flag == 1) {
-        document.getElementById("Box4").value = "X";
-        document.getElementById("Box4").disabled = true;
-        flag = 0;
+    function Function_8() {
+        if (flag == 1) {
+            document.getElementById("Box6").value = "X";
+            document.getElementById("Box6").disabled = true;
+            flag = 0;
+        }
+        else {
+            document.getElementById("Box6").value = "0";
+            document.getElementById("Box6").disabled = true;
+            flag = 1;
+        }
     }
-    else {
-        document.getElementById("Box4").value = "0";
-        document.getElementById("Box4").disabled = true;
-        flag = 1;
+    function Function_9() {
+        if (flag == 1) {
+            document.getElementById("Box7").value = "X";
+            document.getElementById("Box7").disabled = true;
+            flag = 0;
+        }
+        else {
+            document.getElementById("Box7").value = "0";
+            document.getElementById("Box7").disabled = true;
+            flag = 1;
+        }
     }
-}
-function Function_7() {
-    if (flag == 1) {
-        document.getElementById("Box5").value = "X";
-        document.getElementById("Box5").disabled = true;
-        flag = 0;
+    function Function_10() {
+        if (flag == 1) {
+            document.getElementById("Box8").value = "X";
+            document.getElementById("Box8").disabled = true;
+            flag = 0;
+        }
+        else {
+            document.getElementById("Box8").value = "0";
+            document.getElementById("Box8").disabled = true;
+            flag = 1;
+        }
     }
-    else {
-        document.getElementById("Box5").value = "0";
-        document.getElementById("Box5").disabled = true;
-        flag = 1;
+    function Function_11() {
+        if (flag == 1) {
+            document.getElementById("Box9").value = "X";
+            document.getElementById("Box9").disabled = true;
+            flag = 0;
+        }
+        else {
+            document.getElementById("Box9").value = "0";
+            document.getElementById("Box9").disabled = true;
+            flag = 1;
+        }
     }
-}
-function Function_8() {
-    if (flag == 1) {
-        document.getElementById("Box6").value = "X";
-        document.getElementById("Box6").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("Box6").value = "0";
-        document.getElementById("Box6").disabled = true;
-        flag = 1;
-    }
-}
-function Function_9() {
-    if (flag == 1) {
-        document.getElementById("Box7").value = "X";
-        document.getElementById("Box7").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("Box7").value = "0";
-        document.getElementById("Box7").disabled = true;
-        flag = 1;
-    }
-}
-function Function_10() {
-    if (flag == 1) {
-        document.getElementById("Box8").value = "X";
-        document.getElementById("Box8").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("Box8").value = "0";
-        document.getElementById("Box8").disabled = true;
-        flag = 1;
-    }
-}
-function Function_11() {
-    if (flag == 1) {
-        document.getElementById("Box9").value = "X";
-        document.getElementById("Box9").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("Box9").value = "0";
-        document.getElementById("Box9").disabled = true;
-        flag = 1;
-    }
-}
 }
